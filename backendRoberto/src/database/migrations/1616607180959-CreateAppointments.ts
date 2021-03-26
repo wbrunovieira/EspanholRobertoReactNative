37cc3145e7e9ee46287827483ca6b3f28370b588
wbrunovieira <wbrunovieira77@gmail.com>
2021-03-26 17:13:27 +0000

    made the route, model, migration, service for create a user

--- backendRoberto/src/database/migrations/1616607180959-CreateAppointments.ts
+++ backendRoberto/src/database/migrations/1616607180959-CreateAppointments.ts
@@ -1,6 +1,6 @@
 import {MigrationInterface, QueryRunner, Table} from "typeorm";
 
-export class CreateAppointments1616607180959 implements MigrationInterface {
+export default class CreateAppointments1616607180959 implements MigrationInterface {
 
     public async up(queryRunner: QueryRunner): Promise<void> {
         await queryRunner.createTable(
@@ -9,7 +9,7 @@ export class CreateAppointments1616607180959 implements MigrationInterface {
                 columns:[
                     {
                         name:'id',
-                        type:'varchar',
+                        type:'uuid',
                         isPrimary:true,
                         generationStrategy: 'uuid',
                         default:'uuid_generate_v4()',
@@ -23,7 +23,17 @@ export class CreateAppointments1616607180959 implements MigrationInterface {
                         name:'date',
                         type:'timestamp with time zone',
                         isNullable: false,
-                    }
+                    },
+                    {
+                        name:'created_at',
+                        type: 'timestamp',
+                        default: 'now()',
+                    },
+                    {
+                        name:'updated_at',
+                        type:'timestamp',
+                        default:'now()',
+                    },
                 ]
             })
         )
