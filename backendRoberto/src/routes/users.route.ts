446ea2c3fe8c11f9fedeb91fb28ee52e493809c2
wbrunovieira <wbrunovieira77@gmail.com>
2021-04-13 16:03:57 +0100

    added the feature upload user photo

--- backendRoberto/src/routes/users.route.ts
+++ backendRoberto/src/routes/users.route.ts
@@ -1,8 +1,13 @@
-import { Router } from 'express';
+import { response, Router } from 'express';
+import multer from 'multer';
+import uploadConfig from '../config/upload';
 
 import CreateUserService from '../service/CreateUserService';
+import UpdateUserAvatarService from '../service/UpdateUserAvatarService';
+import ensureAuthenticated from '../middlewares/ensureAuthenticated';
 
 const usersRouter = Router();
+const upload = multer(uploadConfig)
 
 interface User {
     name: string;
@@ -28,6 +33,22 @@ usersRouter.post('/', async (request, response) => {
    } catch(err) {
        return response.status(400).json({ error: err.message });
    }
+})
+
+usersRouter.patch('/avatar',ensureAuthenticated, upload.single('avatar'), async (request, response) => {
+    try{
+        const updateUserAvatar = new UpdateUserAvatarService();
+
+        const user = await updateUserAvatar.execute({
+            user_id: request.user.id,
+            avatarFileName: request.file.filename,
+        });
+
+        return response.json(user)
+
+    }catch(err){
+        return response.status(400).json({ error: err.message });
+    }
 })
 
 export default usersRouter;
