95f5e3e2a051dcf481a89c94e3c2e011659125ac
wbrunovieira <wbrunovieira77@gmail.com>
2022-01-24 18:32:00 +0000

    remove the feature appointms, so deleted the files apponitms model, service and route

--- backendRoberto/src/routes/index.ts
+++ backendRoberto/src/routes/index.ts
@@ -1,11 +1,11 @@
 import {    Router } from 'express';
-import appointmentsRouter from './appointments.route';
+
 import usersRouter from './users.route';
 import sessionsRouter from './session.route';
 
 const routes = Router();
 
-routes.use('/appointments', appointmentsRouter);
+
 routes.use('/users', usersRouter);
 routes.use('/sessions', sessionsRouter);
 
