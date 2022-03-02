a25a8565e39b982cc9756e1b6f964cd62b0a03a6
wbrunovieira <wbrunovieira77@gmail.com>
2022-03-02 21:52:27 +0000

    started the login page

---
+++ espanhol-roberto-mobile/App.tsx
@@ -0,0 +1,13 @@
+
+import React from 'react';
+
+import { Login } from './src/pages/Login';
+
+export default function App() {
+  return (
+
+    <Login />
+    
+  );
+};
+
---
+++ espanhol-roberto-mobile/src/pages/Login/styles.ts
@@ -0,0 +1,18 @@
+import styled from 'styled-components/native';
+
+export const Container = styled.View`
+    width: 100%;
+    height: 300px;
+    flex: 1;
+    align-items: center;
+    justify-content: center;
+
+`;
+
+export const Title = styled.Text`
+font-size: 24px;
+
+color: black;
+
+font-weight: bold;
+`;
